326990713453f5ceca04a8362d5bbafc1983fbfb
Laurent <nielbagayao0@gmail.com>
2024-11-13 21:05:18 +0800

    commit1

--- app/src/main/java/com/example/medilertsosfeature/MainActivity.kt
+++ app/src/main/java/com/example/medilertsosfeature/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.medilertsosfeature.R
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
